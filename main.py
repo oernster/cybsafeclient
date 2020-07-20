439388b6647390c76a00a41ebb4a3b8af9e15206
oernster <oliverjernster@hotmail.com>
2020-07-20 22:57:34 +0100

    Edit IP to be that of server again
    The rebase messed it up.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import requests
 
 logging.basicConfig(filename='cybclient.log', level=logging.DEBUG)
 
-SERVER_URL = 'http://127.0.0.1:5000'
+SERVER_URL = 'http://87.254.6.135:5000'
 
 class Client(object):
     def __init__(self):
